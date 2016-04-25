0d7a1edfe9bee32be6df1715c0d50e5dd25ac8e0
Ravinder <ravinderk_1986@yahoo.com>
2016-04-26 00:00:17 +0530

    gh
    g

---
+++ Wise Event/raaaaaViewController.h
@@ -0,0 +1,13 @@
+//
+//  raaaaaViewController.h
+//  Wise Event
+//
+//  Created by MAC on 25/04/16.
+//  Copyright © 2016 Possibility Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface raaaaaViewController : UIViewController
+
+@end
---
+++ Wise Event/raaaaaViewController.m
@@ -0,0 +1,37 @@
+//
+//  raaaaaViewController.m
+//  Wise Event
+//
+//  Created by MAC on 25/04/16.
+//  Copyright © 2016 Possibility Solutions. All rights reserved.
+//
+
+#import "raaaaaViewController.h"
+
+@interface raaaaaViewController ()
+
+@end
+
+@implementation raaaaaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
